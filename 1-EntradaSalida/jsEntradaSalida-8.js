e0d3026866bf7b489a7777980f699e25cdb35071
Marcelodecicco <marcelodecicco73@gmail.com>
2016-11-25 18:44:01 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,21 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
+	//id="numeroDividendo
+	//id="numeroDivisor"
+	var dividendo;
+	var divisor;
+	
+
+	dividendo=document.getElementById('numeroDividendo').value;
+	divisor=document.getElementById('numeroDivisor').value;
+
+	dividendo=parseInt(dividendo);
+	divisor=parseInt(divisor);
+
+	alert("El resto es  "+(dividendo%divisor));
 	
 }
+//funcion modulo, veo si un nro es divisible por otro
+// parseInt no es necesario
+
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,18 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+ 
+	var porcentaje ;
+	var precio=100;
+	var precioFinal;
 
-	
+	var aumento; 
+	aumento=precio *0.21; 
+	precioFinal=precio+aumento;
+	/*aumento=precio *0.21;
+	precioFinal=precio+aumento;*/
+	precioFinal=precio*1.21;
+
 }
+
+// REVISARRRR
