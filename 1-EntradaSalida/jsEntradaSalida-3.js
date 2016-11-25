36a6e8e3de580c08be82eebe21fa21e98687e431
Marcelodecicco <marcelodecicco73@gmail.com>
2016-11-25 17:42:42 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,14 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	//identificador que esta al lado de html. Estructura dom que java scrip puede manejar.
+	//Escritura camel, cuando tenes en mayùscula la segunda palabra.
+	var elNombre
+	elNombre=document.getElementById('elNombre').value;
+alert ("mi nombre");
 }
+//este objeto tiene un atributo. Si no ponemos .value aparece [object HTMLInputElement]
+
+
 
 
