be83f11a0988b180341897745f0e932ae3966dde
Marcelodecicco <marcelodecicco73@gmail.com>
2016-12-05 00:32:42 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,6 +1,18 @@
 function Mostrar()
 {
 //tomo la edad  
+var laedad;
+
+laEdad=document.getElementById('edad').value;
+
+console.log("esto es un test y la edad es: " + edad);
+
+if(laEdad==15){
+
+	alert("niña bonita");
+}
+
+	
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -2,6 +2,17 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
 
+edad=document.getElementById('edad').value;
+
+if( edad > 17){
+
+	alert("es mayor de edad, su edad es:" + edad);
+
+}
+else{
+	alert("usted no es mayor de edad");
+}
 
 }//FIN DE LA FUNCIÓN
