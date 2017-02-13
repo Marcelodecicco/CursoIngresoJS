b91447d33db5c7417fb679392f213655d34ea7a9
Marcelodecicco <marcelodecicco73@gmail.com>
2017-02-13 19:46:16 -0300

    Add files via upload

--- 9-Parciales/6-iteraciones.js
+++ 9-Parciales/6-iteraciones.js
@@ -2,6 +2,37 @@
 function Mostrar()
 {
 	alert("Funciona 6-iteraciones");
+
+var importe;
+
+contador=0;
+
+
+while(contador<7)
+{
+	contador++;
+	importe=prompt("ingrese importe numero "+contador);
+	while(importe<1)
+	{
+		importe=prompt("error, ingrese importe numero "+contador);
+	}
+
+	if(contador==1)
+	{
+		maximo=importe;
+	}
+	if(importe>maximo)
+	{
+		maximo=importe;
+		
+	}
+
+
+
+}
+
+
+
 	
 }
 
