299d58d15254a6e0c341e700e491b916f3646e4d
Marcelodecicco <marcelodecicco73@gmail.com>
2017-02-13 19:03:55 -0300

    Add files via upload

--- 9-Parciales/4-if.js
+++ 9-Parciales/4-if.js
@@ -2,57 +2,75 @@
 function Mostrar()
 {
 	alert("Funciona 4-if");
+	/*
 
-	var numeroUno;
-	var numeroDos;
+	var num1;
+	var num2;
 	var resultado;
-	
 
-	numeroUno=prompt("ingrese numeroUno");
-	numeroDos=prompt("ingrese numeroDos");
-	numeroUno=parseInt(numeroUno);
-	numeroDos=parseInt(numeroDos);
+	num1=prompt("ingrese el primer nro");
+	num2=prompt("ingrese el segundo nro");
+
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+	// solo donde hay suma se usa el parseint
+	// la regla es 1) las variables
+	// 2) leer
+	// 3) 
+
+	if (num1==num2)
+	 {
+	 	resultado=num1*num2
+
+	 }
+	 else
+	 {
+	 	if (num1>num2) 
+	 	{
+	 	resultado=num1-num2;	
+	 	}
+	 	else
+	 	{
+	 	resultado=num1+num2;
+	 	}
+	 }
+	 */
+
+	 //si edad es 12 o menor es un niño
+	 //si es entre 13 y 17 es un adolecente
+	 //si es de 18 en adelante es mayor
+
+	 var edad;
+
+	 edad=prompt("ingrese la edad");
+
+	 if(edad<13)
+	 {
+	 	alert("es un niño");
+	 }
+	 else
+	 {
+	 	if(edad>17)
+	 	{
+	 		alert("es mayor de edad");
+	 	}
+	 	else
+	 	{
+	 		alert("es adolecente");
+
+	 	}
+	 }
 
 
 
-	/*multiplicar=numeroUno*numeroDos;
-	resta=numeroUno-numeroDos;
-	suma=numeroUno+numeroDos;*/
-
-	if (numeroUno==numeroDos) 
-	{
-	   resultado=numeroUno*numeroDos;
-	}
-	else
-	{
-		if(numeroUno>numeroDos)
-		{
-			resultado=numeroUno+numeroDos;
-		}
-		else
-		{
-			resultado=numeroUno-numeroDos;
-		}
-	} 
-		
 
 
 
-	/*if (numeroUno==numeroDos) 
-	{
-	   resultado=numeroUno*numeroDos;
-	}
-	if (numeroUno>numeroDos) 
-		{
-	    resultado=numeroUno+numeroDos
 
-		}
-	if(numeroUno<numeroDos)
-	{
-		resultado=numeroUno-numeroDos
 
-	}	*/ 	
-		document.write(resultado);
+
+
+	 document.write(resultado);
 
 
 	
--- 9-Parciales/5-switch.js
+++ 9-Parciales/5-switch.js
@@ -2,6 +2,43 @@
 function Mostrar()
 {
 	alert("Funciona 5-Switch");
+
+	/*
+	var mes;
+
+	mes=prompt("ingrese el mes");
+
+	if(mes=="enero" || mes=="febrero")
+	{
+		alert("veranito");
+	}
+	else
+	{
+		alert("extraño enero y febrero");
+	}
+
+*/
+
+	var mes;
+	mes=prompt("ingrese el mes");
+
+	switch(mes)
+	{
+	   case "Enero":
+	   case "febrero":
+	   alert("veranito");
+	   break;
+
+	   default:
+	   alert("extraño");
+
+	}
+
+
+ 
+
+
+
 	
 }
 
