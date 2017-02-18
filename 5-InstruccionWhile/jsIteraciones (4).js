fbaea99f30cfecce43293dba8e90cb05d3f64adc
Marcelodecicco <marcelodecicco73@gmail.com>
2017-02-18 16:35:16 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,33 @@
 function Mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	//var numero = prompt("ingrese un número entre 0 y 9.");
+
+	var numero;
+	numero=prompt("ingrese un numero entre 0 y 9");
+	numero=parseInt(numero);
 
 
+// siempre va lo malo 
+	while(numero<0 || numero>10)
+	{	//estas dos lineas van igual que las dos de arriba
+		numero=prompt("ingrese un numero entre 0 y 9");
+		numero=parseInt(numero);
+	}
+	alert("ok"+numero);
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,41 @@
 function Mostrar()
 {
+	/*var contador=0;
+	var acumulador=0;
+	var numeroIngresado;
+
+	while(contador<5)
+	{
+		contador++;
+		numeroIngresado=prompt("ingrese numero " + contador);
+		numeroIngresado=numeroIngresado;
+		acumulador=acumulador+parseInt(numeroIngresado);
+	}
+
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/5;
+*/
 
 	var contador=0;
+	var numero;
 	var acumulador=0;
+	while(contador<5)
+	{
+		
+		console.log(contador);
+		contador=contador+1;
+		numero=prompt("ingrese un numero entre 0 y 9");
+		numero=parseInt(numero);
+		acumulador=acumulador+numero;
 
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	}
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/5;
+
+// 1-declarar
+//2-condicion
+//3-modificar contador
 
 }//FIN DE LA FUNCIÓN
